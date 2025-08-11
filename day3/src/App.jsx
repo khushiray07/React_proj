87cd8389eec9bbcd3c204f4c51837c7cbadec1dc
khushiray07 <khushiray972@gmail.com>
2025-08-11 20:08:37 +0530

    created custom hooks

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -3,15 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
+import useFetch from './useFetch';
 
 function App() {
+  const { data, loading } = useFetch('https://jsonplaceholder.typicode.com/todos/1');
 
   return (
-    <>
-      <h1>day3</h1>
-      <Counter/>
-    </>
-  )
+    <div className="App">
+      <h1>State Management Demo</h1>
+      <Counter />
+      {loading ? <p>Loading...</p> : <p>Title: {data.title}</p>}
+    </div>
+  );
 }
 
 export default App
